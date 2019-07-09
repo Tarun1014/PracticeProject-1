addef72f5631611636bd4be89bed276a587385f1
train <tarunkumar1014@gmail.com>
2019-07-09 12:11:37 +0530

    New Commit

---
+++ eclipse-workspace/Training/src/Maven/Calculate.java
@@ -0,0 +1,14 @@
+package Maven;
+
+public class Calculate {
+
+	public static void main(String args[]) {
+		System.out.println("Hello Tarun!How are you?");
+		Addition add=new Addition();
+		Subtraction sub=new Subtraction();
+		System.out.println(sub.Minus(10,15));
+		System.out.println(add.plus(10, 15));
+		
+							
+	}
+}
